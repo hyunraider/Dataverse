be4c8e4adbb6c1419218f200648496ef6c6e0f37
hyunraider <hyunraider@yahoo.com>
2016-12-03 12:32:34 -0600

    inital working demo

--- app.py
+++ app.py
@@ -1,6 +1,7 @@
 from flask import Flask, url_for, render_template, jsonify, request, json
 from flask.ext.mysql import MySQL
-from wikimarkup import parse
+import wikipedia
+
 
 mysql = MySQL()
 app = Flask(__name__)
@@ -10,15 +11,76 @@ app.config['MYSQL_DATABASE_DB'] = 'wiki'
 app.config['MYSQL_DATABASE_HOST'] = 'localhost'
 mysql.init_app(app)
 
-html = ""
-db = []
-
 @app.route('/')
 def hello_world():
-	return render_template("index.html", dbs=db)
+	return render_template("index.html")
+
+@app.route('/insert', methods=['POST'])
+def insertword():
+    conn = mysql.connect()
+    cursor = conn.cursor()
+    keyword = request.form.get('keyword')
+    insertword = request.form.get('insert')
+    catword = request.form.get('cat')
+
+    sql_query = "INSERT INTO `TABLE 2` VALUES ('" + keyword + "', '" + catword + "')"
+    cursor.execute(sql_query)
+    conn.commit()
+    sql_query = "INSERT INTO `TABLE 2` VALUES ('" + insertword + "', '" + catword + "')"
+    cursor.execute(sql_query)
+    conn.commit()
+    sql_query = "SELECT * FROM `TABLE 1` WHERE title='" + insertword + "'"
+    cursor.execute(sql_query)
+    data = cursor.fetchall()
+    if len(data) == 0:
+        sql_query = "INSERT INTO `TABLE 1` VALUES ('" + insertword + "', 'KEK', 'This is a feature to be implemented!', 'KEK')"
+        cursor.execute(sql_query)
+        conn.commit()
+        print "inserted"
+
+    return json.dumps([])
+
 
 @app.route('/query', methods=['POST'])
 def testquery():
+    keyword = request.form.get('keyword')
+
+    cursor = mysql.connect().cursor()
+    print "starting mysql"
+
+    sql_query = "SELECT title FROM (SELECT title FROM (SELECT * FROM (SELECT t1.category AS cat1 FROM `TABLE 2` AS t1 WHERE t1.title = '" + keyword  + "') AS A CROSS JOIN (SELECT t2.category AS cat2, t2.title AS title FROM `TABLE 2` AS t2 WHERE t2.title<>'" + keyword + "') AS B) AS C WHERE cat1=cat2) AS D GROUP BY title HAVING COUNT(title)>2"
+    print sql_query
+    cursor.execute(sql_query)
+    data = cursor.fetchall()
+    myList = []
+    print str(data)
+    for z in data:
+        myList.append(z[0]);
+    print myList
+
+    returnList = []
+    for x in myList:
+        temp_sql_query = "SELECT * FROM `TABLE 1` WHERE title='" + x + "'"
+        print temp_sql_query
+        cursor.execute(temp_sql_query)
+        newData = cursor.fetchone()
+        someList = []
+        for y in newData:
+            someList.append(y)
+
+        returnList.append(someList)
+
+    return json.dumps(returnList)
+
+    """
+    currData = {}
+    currData["title"] = curr.title
+    currData["summary"] = curr.summary.split('.')[0]
+    currData["categories"] = curr.categories
+    currData["pictures"] = curr.images
+    return json.dumps(currData)
+    """
+    """
 	cursor = mysql.connect().cursor()
 	print "starting mysql"
     	keyword = request.form.get('keyword')
@@ -27,13 +89,13 @@ def testquery():
 	substr = "SELECT * from page where page_title='"
     	fullquery = substr + keyword + "'";
     	result = cursor.execute(fullquery)
-	
+
 	print "query done"
 	print result
 
 	data = cursor.fetchone()
 	print data
-	
+
 	db.append(data);
 	#old_id = data[6]
 	#substr = "SELECT old_text from text where old_id='"
@@ -47,5 +109,5 @@ def testquery():
 	#db.append(data)
 	#db.append(parse(data))
 	return json.dumps(data)
-
+	"""
 app.run(host='0.0.0.0')
--- static/main.js
+++ static/main.js
@@ -1,3 +1,4 @@
+/*
 $("#querybox").click(function(e){
 	e.preventDefault();
 	console.log("querying");
@@ -12,10 +13,77 @@ $("#querybox").click(function(e){
 		success: function(response){
 			console.log(typeof(response));
 			console.log(response);
+			
+			$("#imageBox").append("<img src='" + response["pictures"][0] + "'/>");
+			
 			reload();
 		},
 		error: function(error){
 			console.log(error);
 		} 
 	});
+});
+*/
+var $grid = $("#masonry").masonry({
+            
+});
+
+$("#insertbox").click(function(e){
+    e.preventDefault();
+    var keyword = $('#queryword').val();
+    var insert = $('#insertstuff').val();
+    var cat = $('#category').val();
+    $.ajax({
+        url:'/insert',
+        data: {
+            keyword: keyword,
+            insert: insert,
+            cat: cat
+        },
+        type: 'POST',
+        success: function(response){
+            console.log("insert success!");
+        
+        },
+        error: function(error){
+            console.log(error);
+        }
+    });
+
+
+});
+
+$("#querybox").click(function(e){
+    e.preventDefault();
+    $("#masonry").empty();
+    var keyword = $('#queryword').val();
+    $.ajax({
+        url:'/query',
+        data: {
+            keyword: keyword
+        },
+        type: 'POST',
+        success: function(response){
+            console.log("success!!");
+
+            var myJSON = JSON.parse(response);
+            
+            for (x in myJSON){
+                var list = myJSON[x]
+                console.log(list);
+                var $newblock = $(document.createElement('div'));
+                $newblock.addClass("col-sm-4");
+                $newblock.css({"width":"250px", "display":"inline-block" ,"border": "2px black solid", "background-color":"red"});
+                $newblock.append("<h4>" + list[0] + "  [UPVOTE] [DOWNVOTE]</h4>");
+                console.log(response);
+                console.log(myJSON.summary);
+                $newblock.append("<p style='font-size:10px'>" + list[2] + "</p>");
+                setTimeout($grid.append($newblock).masonry('appended', $newblock, true), 2000);
+            }
+            $("#masonry").masonry(); 
+        },
+        error: function(error){
+            console.log(error);
+        }
+    });
 });
